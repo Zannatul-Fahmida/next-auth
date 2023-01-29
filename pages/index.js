cc3851f23aa066cef2d0702f0266104ac31a47b9
zannatul <jannatulfahmida15@gmail.com>
2023-01-29 19:51:47 +0600

    github authentication added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "../styles/Home.module.css";
+import { useSession } from "next-auth/react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <>
       <Head>
@@ -26,7 +28,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -40,6 +42,7 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
+          {session ? <h1 style={{paddingRight: '10px'}}>{session.user.name}, </h1> : ""}
           <Image
             className={styles.logo}
             src="/next.svg"
@@ -119,5 +122,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }

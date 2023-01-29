721572d720356b713344466f333f10fb5f22a6a1
zannatul <jannatulfahmida15@gmail.com>
2023-01-29 20:34:14 +0600

    url updated

--- pages/blog.js
+++ pages/blog.js
@@ -13,7 +13,7 @@ export async function getServerSideProps(context) {
   if (!session) {
     return {
       redirect: {
-        destination: "/api/auth/signin?callbackUrl=http://localhost:3000/blog",
+        destination: `/api/auth/signin?callbackUrl=${context.req.headers.referer}`,
         permanent: false,
       },
     };
